7f4faabb9a1100fbbb5dac3dac22e80593c86c5a
seryu <hanseryu136@gmail.com>
2026-05-01 14:45:27 +0900

    add Ch01 study

---
+++ Ch01/src/sub1/Hello.java
@@ -0,0 +1,19 @@
+package sub1;
+
+/*
+ * 날짜 : 2026/04/27
+ * 이름 : 한세류
+ * 내용 : 1장 Java 개발 환경 구축 실습
+ */
+
+public class Hello {
+	
+	public static void main(String[] args) {
+		
+		// 모든 프로그래밍 언어의 상징적인 입문 문자열
+		System.out.println("Hello World!");
+		System.out.println("Hello Java!");
+		
+	} 
+	
+}
