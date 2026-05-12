c1be8598ca7555d77dafca0c309ba4c7d381d1b9
seryu <hanseryu136@gmail.com>
2026-05-12 12:24:45 +0900

    adf

---
+++ Ch08/src/sub1/FruitBox.java
@@ -0,0 +1,16 @@
+package sub1;
+
+//치환 변수 T를 갖는 제네릭 클래스
+public class FruitBox<T> {	
+	
+	private T fruit;
+	
+	public T getFruit() {
+		return fruit;
+	}
+	
+	public void setFruit(T fruit) {
+		this.fruit = fruit;
+	}
+	
+}
